0080e864ceea3d3b4e4c7436d47caf555a71792d
Lane Garner <b.lane.garner@gmail.com>
2020-10-18 17:38:25 -0500

    veg burger working

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 // $(document).ready(function(){
 //     $('.aniview').AniView();
 // });
+
+function displayMenu() {
+const veggieBurgerMenu = document.getElementById("nav");
+const closed = document.querySelector('#veggie-closed');
+const open = document.querySelector('#veggie-open');
+
+console.log('click');
+if (veggieBurgerMenu.style.display == "none" || veggieBurgerMenu.style.display == "") {
+veggieBurgerMenu.style.display = "flex";
+open.style.display= "block";
+closed.style.display= "none";
+} else {
+    veggieBurgerMenu.style.display = "none";
+    open.style.display= "none";
+    closed.style.display= "block";
+}  
+}
